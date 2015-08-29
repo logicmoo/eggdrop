b085c5567c2c16b14bed269abf5e5f86c92c2ccd
logicmoo <logicmoo@gmail.com>
2015-08-28 23:39:23 -0700

    a fake pack file

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(logicmoo_base).
+version('1.1.1').
+author('Douglas R. Miles','logicmoo@gmail.com').
+title('Support for reactive [agent] programming using forward-chaining').
+keywords([assertions,fc,reactive,dynamic,runtime,logicmoo]).
+home('http://prologmoo.com/packs').
+download('https://packs.prologmoo.com/packs/logicmoo_base/release/logicmoo_base-1.1.1.tgz').
+
